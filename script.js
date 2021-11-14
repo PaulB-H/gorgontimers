9951aeb129ec7d624214704d3a7c7fb9843720f2
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2021-11-14 01:39:13 -0500

    script: add dungeonArray w/ Goblin Dungeon & its chests

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+const dungeonArray = [
+  {
+    name: "Goblin Dungeon",
+    chestGroups: [
+      {
+        name: "Pre-Fog Chests",
+        hours: 3,
+        numOfChests: 6,
+        lastLooted: [null, null, null, null, null, null],
+        timeRemaining: [null, null, null, null, null, null],
+      },
+      {
+        name: "Red Crystal",
+        hours: 1,
+        numOfChests: 2,
+        lastLooted: [null, null],
+        timeRemaining: [null, null],
+      },
+      {
+        name: "Green Crystal",
+        hours: 1,
+        numOfChests: 1,
+        lastLooted: [null],
+        timeRemaining: [null],
+      },
+    ],
+  },
+];
